b2884308d2529ce390f4077eb93add2cd90c5bf1
Mohammed Babelly <mohammedababelly@gmail.com>
2021-10-27 05:48:08 +0300

    call JAR file

--- app/src/main/java/com/example/testliveedgedetection/MainActivity.java
+++ app/src/main/java/com/example/testliveedgedetection/MainActivity.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.adityaarora.liveedgedetection.activity.ScanActivity;
 import com.adityaarora.liveedgedetection.constants.ScanConstants;
 import com.adityaarora.liveedgedetection.util.ScanUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+import hexacode.Code;
+import hexacode.Functionality;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE = 101;
@@ -38,6 +47,18 @@ public class MainActivity extends AppCompatActivity {
                     Bitmap baseBitmap = ScanUtils.decodeBitmapFromFile(filePath, ScanConstants.IMAGE_NAME);
                     scannedImageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
                     scannedImageView.setImageBitmap(baseBitmap);
+
+                    Functionality functionality = new Functionality();
+                    try {
+                        File file = new File(filePath);
+                        Code code = functionality.ConvertHXtoDC(file);
+                        Log.d("Code", code.toString());
+
+                        Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+                        //bitmap.
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             } else if(resultCode == Activity.RESULT_CANCELED) {
                 finish();
